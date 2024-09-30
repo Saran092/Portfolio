0f4b21e20b481c6fcd5263ec009bbafd5d2379c7
Saran K <146074509+Saran092@users.noreply.github.com>
2024-09-30 06:31:49 +0530

    Add files via upload

---
+++ saran.js
@@ -0,0 +1,11 @@
+function showSidebar()
+{
+	const sidebar = document.querySelector('.sideMenu');
+	sidebar.style.display='flex';
+}
+
+function hideSidebar()
+{
+	const sidebar = document.querySelector('.sideMenu');
+	sidebar.style.display='none';
+}
